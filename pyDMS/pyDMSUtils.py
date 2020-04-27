3e4c118a48f339a3bbbea79321e3ab1ad791e637
hectornieto <hector.nieto.solana@gmail.com>
2020-04-27 09:17:23 +0200

    Do not print saving message when saving into memory

--- pyDMS/pyDMSUtils.py
+++ pyDMS/pyDMSUtils.py
@@ -141,6 +141,7 @@ def saveImg(data, geotransform, proj, outPath, noDataValue=None, fieldNames=[]):
             ds.close()
             ds = gdal.Open('NETCDF:"'+outPath+'":'+fieldNames[0])
 
+
     if outPath != "MEM":
         print('Saved ' + outPath)
 
